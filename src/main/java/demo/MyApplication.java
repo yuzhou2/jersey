8ed86c048f87a5e88b102deb83d438ad7b4f6c14
yuzhou <yuzhou@oath.com>
2018-07-23 16:02:51 +0800

    minor change

---
+++ src/main/java/demo/MyApplication.java
@@ -0,0 +1,14 @@
+package demo;
+
+import javax.ws.rs.ApplicationPath;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+@ApplicationPath("/")
+public class MyApplication extends ResourceConfig {
+
+    public MyApplication() {
+        packages("demo");
+    }
+
+}
